37d0ba998fc216d76f0b180d8413989ff53c98ae
bikund2017 <bikund2017@outlook.com>
2026-08-03 22:33:51 +0530

    data types

---
+++ data_types/primitive.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void greet() {
+    printf("Hello, welcome!\n");
+}
+
+int main() {
+    // integer data type
+    int var = 42;
+
+    // printf("var = %d", var);
+
+    // Character data type
+
+    char ch = 'A';
+
+    // printf("ch = %c", ch);
+
+    // Float data type
+
+    float val = 12.45;
+
+    // printf("val = %f", val);
+
+    // Double data type
+
+    double big = 1.4521;
+
+    // printf("big = %lf", big);
+
+    // Void data type
+
+    greet();
+
+    return 0;
+}
